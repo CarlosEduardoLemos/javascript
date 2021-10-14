07ea482406fd9d26ffe098a9d962b6b940b43260
Carlos Eduardo De Souza Lemos <92192426+CarlosEduardoLemos@users.noreply.github.com>
2021-10-14 14:21:10 -0300

    Update script.js

--- desafios/d004/script.js
+++ desafios/d004/script.js
@@ -1,8 +1,9 @@
 function comprar(){
     let produto = (window.prompt('Qual produto você está comprando? '))
-    let preco = Number(window.prompt(`Quanto custa ${produto} que você está comprando?`))
-    let dinheiro = Number(window.prompt(`Qual foi o valor que você deu para pagar ${produto}? `))
+    let preco = Number(window.prompt(`Quanto custa ${produto} que você está comprando?`).replace(',', '.'))
+    let dinheiro = Number(window.prompt(`Qual foi o valor que você deu para pagar ${produto}? `).replace(',', '.'))
 
     let som = dinheiro - preco
-    window.alert(`Você comprou ${produto} que custou ${preco}. Deu ${dinheiro} em dinheiro e vai receber ${som} de troco. Volte sempre`)
+
+    window.alert(`Você comprou ${produto} que custou R$${preco.toFixed(2).replace('.', ',')}. Você deu R$${dinheiro.toFixed(2).replace('.', ',')} em dinheiro e vai receber R$${som.toFixed(2).replace('.', ',')} de troco. Volte sempre`)
 }
