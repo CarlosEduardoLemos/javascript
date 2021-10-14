8dba9c5564c32feb3f32148b939aff831a3872d7
Carlos Eduardo De Souza Lemos <92192426+CarlosEduardoLemos@users.noreply.github.com>
2021-10-14 13:14:02 -0300

    Atualizando

---
+++ desafios/d001/script.js
@@ -0,0 +1,9 @@
+function clickb1() {
+    window.alert('Você clicou no 1º botão!')
+}
+function clickb2() {
+    window.alert('Você clicou no 2º botão!')
+}
+function clickb3() {
+    window.alert('Você clicou no 3º botão!')
+}
---
+++ desafios/d002/script.js
@@ -0,0 +1,5 @@
+function clickb1() {
+    var nome = window.prompt('Qual o seu nome? ')
+    var idade = window.prompt(`Olá, ${nome} qual a sua idade?`)
+    window.alert(`Acabei de conhecer ${nome}, que tem ${idade} anos de idade!`)
+}
